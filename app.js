707ef059a32a5d93d2153388ba29dbd169c2f189
ImJopeh <jopedev@gmail.com>
2025-07-17 21:10:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,16 +7,6 @@ const firebaseConfig = {
   appId: "1:864862750726:web:5e9ed7379e633df5e0db87"  
 }
 
-// Konfigurasi Firebase
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
